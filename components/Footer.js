5df415205af401e8a088f613ca21689b890e0186
AnkitDutta <39426251+AnkitDutta@users.noreply.github.com>
2022-10-22 14:22:59 +0530

    eslintric

--- components/Footer.js
+++ components/Footer.js
@@ -6,35 +6,35 @@ function Footer() {
      px-32 py-14 bg-gray-100 text-gray-700'>
         <div className='space-y-4 text-xs'>
             <h5 className='font-bold'>COMMUNITY</h5>
-            <p>Accessibility</p>
-            <p>This is not a real site</p>
-            <p>Trying to make a clone</p>
-            <p>Looking for internships</p>
-            <p>Try it out</p>
+            <p className='cursor-pointer'>Accessibility</p>
+            <p className='cursor-pointer'>This is not a real site</p>
+            <p className='cursor-pointer'>Trying to make a clone</p>
+            <p className='cursor-pointer'>Looking for internships</p>
+            <p className='cursor-pointer'>Try it out</p>
         </div>
         <div className='space-y-4 text-xs '>
             <h5 className='font-bold'>MOST</h5>
-            <p>React</p>
-            <p>Presents</p>
-            <p>Full Stack Course</p>
-            <p>Good Project</p>
-            <p>Try it</p>
+            <p className='cursor-pointer'>React</p>
+            <p className='cursor-pointer'>Presents</p>
+            <p className='cursor-pointer'>Full Stack Course</p>
+            <p className='cursor-pointer'>Good Project</p>
+            <p className='cursor-pointer'>Try it</p>
         </div>
         <div className='space-y-4 text-xs '>
             <h5 className='font-bold'>SUPPORT</h5>
-            <p>Help Center</p>
-            <p>Trust and Safety</p>
-            <p>Youtube Algorithm</p>
-            <p>Easter Eggs</p>
-            <p>Airbnb</p>
+            <p className='cursor-pointer'>Help Center</p>
+            <p className='cursor-pointer'>Trust and Safety</p>
+            <p className='cursor-pointer'>Youtube Algorithm</p>
+            <p className='cursor-pointer'>Easter Eggs</p>
+            <p className='cursor-pointer'>Airbnb</p>
         </div>
         <div className='space-y-4 text-xs'>
             <h5 className='font-bold'>ABOUT</h5>
-            <p>How Airbnb works</p>
-            <p>Newsroom</p>
-            <p>Investors</p>
-            <p>Airbnb Plus</p>
-            <p>Airbnb Luxe</p>
+            <p className='cursor-pointer'>How Airbnb works</p>
+            <p className='cursor-pointer'>Newsroom</p>
+            <p className='cursor-pointer'>Investors</p>
+            <p className='cursor-pointer'>Airbnb Plus</p>
+            <p className='cursor-pointer'>Airbnb Luxe</p>
         </div>
     
         
